08a4fe72eea00b2febb63e2e955838d39a813d0e
DVorortnoy <dimchik505@gmail.com>
2023-03-28 18:40:22 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import Form from './Form/Form';
+import ContactList from './ContactList/ContactList';
+import Section from './Section/Section';
+import Filter from './Filter/Filter';
+import { AppStyled } from './App.styled';
+
+export class App extends Component {
+  state = {
+    contacts: [
+    ],
+    filter: '',
+  };
+
+  createNewContact = currentValue => {
+    const alreadyAdded = this.state.contacts.some(
+      obj => obj.name === currentValue.name
+    );
+    alreadyAdded
+      ? alert(`${currentValue.name} is already in contacts`)
+      : this.setState(prevState => {
+        const newContact = {
+          name: currentValue.name,
+          id: nanoid(),
+          number: currentValue.number,
+        };
+        return { contacts: [...prevState.contacts, newContact] };
+      });
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(
+        contact => contact.id !== contactId
+      ),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  render() {
+    const normalizedFilter = this.state.filter.toLowerCase();
+    const filtredContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+    return (
+      <AppStyled>
+        <Section title="Phonebook">
+          <Form onSubmit={this.createNewContact} />
+        </Section>
+        <Section title="Contacts">
+          <Filter
+            value={this.state.filter}
+            onChange={this.changeFilter}
+          />
+          <ContactList
+            contacts={filtredContacts}
+            onDelete={this.deleteContact}
+          />
+        </Section>
+      </AppStyled>
+    );
+  }
+}
